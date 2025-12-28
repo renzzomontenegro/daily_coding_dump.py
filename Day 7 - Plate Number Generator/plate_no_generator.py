d470a4e751205feb9f7a82fff52a3e8508169fd4
John Renzzo C. Montenegro <johmontenegro@my.cspc.edu.ph>
2025-12-29 01:10:16 +0800

    added clear comments and constraints

--- Day 7 - Plate Number Generator/plate_no_generator.py
+++ Day 7 - Plate Number Generator/plate_no_generator.py
@@ -3,17 +3,48 @@ import string
 
 def generate_plate_number():
     def generate_letters():
+        """Generates a list of random letters"""
         rand_letter = random.choices(string.ascii_uppercase, k=3)
-        appended_letter = "".join(rand_letter)
-        return appended_letter
+
+        """Constraint: No Repeating Letters (ex. AAA)"""
+        if rand_letter[0] == rand_letter[1]:
+            rand_letter.remove(rand_letter[1])
+            new_letter = random.choices(string.ascii_uppercase)
+            rand_letter.insert(1, new_letter[0])
+
+        elif rand_letter[1] == rand_letter[2]:
+            rand_letter.remove(rand_letter[2])
+            new_letter = random.choices(string.ascii_uppercase)
+            rand_letter.insert(2, new_letter[0])
+
+        """Combine Letters"""
+        appended_letters = "".join(rand_letter)
+
+        return appended_letters
 
     def generate_numbers():
+        """Generates a list of random numbers"""
         rand_num = random.choices(string.digits, k=4)
-        appended_num = "".join(rand_num)
-        return appended_num
 
+        """Constraint: No Repeating Numbers (ex. 111)"""
+        if rand_num[0] == rand_num[1]:
+            rand_num.remove(rand_num[1])
+            new_num = random.choices(string.digits)
+            rand_num.insert(1, new_num[0])
+
+        elif rand_num[1] == rand_num[2]:
+            rand_num.remove(rand_num[2])
+            new_num = random.choices(string.digits)
+            rand_num.insert(2, new_num[0])
+
+        """Combine Numbers"""
+        appended_num = "".join(rand_num)
+
+        return appended_num
+    
+    """Print Plate Number"""
     print(f'\n{generate_letters()}-{generate_numbers()}')
 
-
-
-generate_plate_number()
+"""Simulate Plate Number Generation"""
+for i in range(1000):
+    generate_plate_number()
