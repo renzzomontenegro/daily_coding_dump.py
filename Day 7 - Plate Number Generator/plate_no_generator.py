74b3d5e5b85bdd615d52fa72f94d9ead4b3070d5
John Renzzo C. Montenegro <johmontenegro@my.cspc.edu.ph>
2025-12-29 00:30:52 +0800

    fixed messy code

--- Day 7 - Plate Number Generator/plate_no_generator.py
+++ Day 7 - Plate Number Generator/plate_no_generator.py
@@ -2,15 +2,17 @@ import random
 import string
 
 def generate_plate_number():
-    rand_letter = random.choices(string.ascii_uppercase, k=3)
-    appended_letter = "".join(rand_letter)
+    def generate_letters():
+        rand_letter = random.choices(string.ascii_uppercase, k=3)
+        appended_letter = "".join(rand_letter)
+        return appended_letter
 
-    rand_num = string.digits
+    def generate_numbers():
+        rand_num = random.choices(string.digits, k=4)
+        appended_num = "".join(rand_num)
+        return appended_num
 
-    rand_num = random.choices(rand_num, k=4)
-    appended_num = "".join(rand_num)
-
-    print(f'\n{appended_letter}-{appended_num}')
+    print(f'\n{generate_letters()}-{generate_numbers()}')
 
 
 
